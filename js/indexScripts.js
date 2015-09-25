18ffbb4ce56a9d93cf4a30aaede32b33c7959c8f
john <johnhuffman@Johns-MacBook-Air.local>
2015-09-24 21:30:57 -0600

    ready for QA

--- js/indexScripts.js
+++ js/indexScripts.js
@@ -1,7 +1,5 @@
 function findSubstring()
 {
-	console.log('about to start the search!');
-
 	var stringToSearch = $('#stringToSearch').val();
 	var subString = $('#subString').val();
 
@@ -16,12 +14,19 @@ function findSubstring()
 	var splitSubString = subString.split('');
 
 	//find the minimum window and set up search
-	for(i = 0; i <= stringToSearch.length - subString.length; i++)
+	for(i = 0; i <= stringToSearch.length; i++)
 	{
 		//build a substring to check
 		var subArray = splitStringToSearch.slice(i, i + subString.length);
 
+		//skip the last few checks because they are too small
+		if(subArray.length < subString.length)
+		{
+			continue;
+		}
+
 		//look for each character in the substring
+		//console.log(subArray);
 		var foundCharacters = 0;
 		for(currentCharacter in splitSubString)
 		{
@@ -30,11 +35,68 @@ function findSubstring()
 				//found a character
 				foundCharacters++;
 			}
+		}
+		if(foundCharacters == subString.length)
+		{
+			//found all the characters in this window!
+			$('#messageCenter').html('Sweet! I found the string! The window is ' + subArray.join(''));
+			return true;
+		}
+		foundCharacters = 0;
+	}
+
+	//if it gets this far the letters aren't together and the window needs to grow
+	var windowLengthModifier = 1;
+	var currentSmallestWindow = '';
+	for(i = 0; i <= stringToSearch.length; i++)
+	{
+		for(j = 0; j < stringToSearch.length; j++)
+		{
+			//build a substring to check
+			var subArray = splitStringToSearch.slice(i, j + subString.length + windowLengthModifier);
+			
+			//skip the last few checks because they are too small
+			if(subArray.length < subString.length + windowLengthModifier)
+			{
+				continue;
+			}
+
+			console.log(subArray);
+			//look for each character in the substring
+			var foundCharacters = 0;
+			for(currentCharacter in splitSubString)
+			{
+				if(subArray.indexOf(splitSubString[currentCharacter]) != -1)
+				{
+					//found a character
+					foundCharacters++;
+				}
+			}
 			if(foundCharacters == subString.length)
 			{
 				//found all the characters in this window!
-				console.log(subArray.join(''));
+				if(currentSmallestWindow == '')
+				{
+					currentSmallestWindow = subArray.join('');
+				}
+				else if(currentSmallestWindow.length > subArray.join('').length)
+				{
+					currentSmallestWindow = subArray.join('');
+				}
+				//return true;
 			}
+			foundCharacters = 0;
+			windowLengthModifier++;
 		}
+		windowLengthModifier = 0;
+	}
+	//finished the search so post the result
+	if(currentSmallestWindow == '')
+	{
+		$('#messageCenter').html('Prepare for doom!  I didn\'t find the string!');
+	}
+	else
+	{
+		$('#messageCenter').html('Sweet! I found the string! The window is ' + currentSmallestWindow);
 	}
 }
